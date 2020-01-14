99fc63c1ec9f4e11e5fe2d4d521d3c58f18909a4
Nytohan <nytohan.rarish@gmail.com>
2020-01-13 23:20:58 -0800

    Add files via upload

--- NodeController/app.js
+++ NodeController/app.js
@@ -1,1 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const hbs = require('express-handlebars');
+const session = require('express-session')
 
+
+var indexRouter = require('./routes/index');
+
+
+var app = express();
+
+app.use(session({
+  'secret': 'r0X4E0WC1OydKKzY1RKll6j3TQNiQqzw',
+}))
+
+// view engine setup
+app.engine('hbs', hbs({
+  extname:'hbs',
+  defaultLayout: "layout",
+  layoutsDir: __dirname + '/views/layouts/',
+  helpers: {ifCond: function (v1, operator, v2, options) {
+        console.log(v1 + operator + v2)
+        switch (operator) {
+            case '==':
+                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+            case '===':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            case '!=':
+                return (v1 != v2) ? options.fn(this) : options.inverse(this);
+            case '!==':
+                return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+            case '<':
+                return (v1 < v2) ? options.fn(this) : options.inverse(this);
+            case '<=':
+                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+            case '>':
+                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+            case '>=':
+                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+            case '&&':
+                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+            case '||':
+                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+            default:
+                return options.inverse(this);
+        }
+  }
+}}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
